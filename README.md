d8c44d4e02f3e20a5005bc830d76ac47851481c9
melbaa <none@no-reply>
2014-12-13 12:52:26 +0200

    readme

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 An extensible IRC framework built on top of ZeroMQ.
 
+# Features
+* main components communicating via ZeroMQ, meaning they can be started separately on different machines
+* handle ssl and password protected servers and nicks
+* plugin architecture
+* multiple server support
+* no busy loops
+
 # Install
 get python 3.4  
 pip install pyzmq  
@@ -16,20 +23,13 @@ see the examples in ircfw/plugins.
 a main.py holds the entry point to a plugin (just a convention)  
 All plugins need unique triggers.  
 
-# About IRC
-architecture http://www.irchelp.org/irchelp/rfc/rfc2810.txt  
-client protocol http://www.networksorcery.com/enp/rfc/rfc2812.txt  
-
 # Why make this
 * IRC is a relatively simple text protocol, easy to debug
 * have more than just a mental framework for a distributed system
 * practice programming abstraction and refactoring
 * mostly a proof of concept and for learning ZeroMQ aka. toy project
 
-# Features
-* main components communicating via ZeroMQ, meaning they can be started separately on different machines
-* handle ssl and password protected servers and nicks
-* plugin architecture
-* multiple server support
-* no busy loops
+# About IRC
+architecture http://www.irchelp.org/irchelp/rfc/rfc2810.txt  
+client protocol http://www.networksorcery.com/enp/rfc/rfc2812.txt  
 
--- ircfw/plugins/_BROKEN/plz/main.py
+++ ircfw/plugins/_BROKEN/plz/main.py
@@ -1,3 +1,7 @@
+# DEAD CODE :)
+# TODO thinking how to write it as plugins or something else
+
+
 class plugin():
 
     def __init__(self, bot):
@@ -14,3 +18,69 @@ class plugin():
 
     def use(self, rawcommand):
         return "nothing to do '" + rawcommand + "' received"
+
+
+
+import urllib.parse
+import urllib.request
+import urllib.error
+import re
+import unicodedata
+import io
+import random
+import os
+import string
+import collections
+import socket
+
+import lxml.etree
+import lxml.html
+
+import ircfw.constants as constants
+import ircfw.parse as parse
+
+def plzprivmsg(self):
+    to, self.trailing = parse.get_word(self.trailing)
+    self.queue_reply("PRIVMSG "+ to + ' ' + ':' + self.trailing)
+
+def plzjoin(self):
+    chan, self.trailing = parse.get_word(self.trailing)
+    self.join_chan(chan)
+
+
+def plzflush(self):
+    self._file.flush()
+    self.privmsg(self.sender[0], "log is the file "
+                 + self._file.name + ' in ' + os.getcwd())
+
+
+"""
+this thing works but looks baaaaaad so i removed the command
+it'll stay here for reference
+"""
+def topic(self):
+    if self.nick != self.params[0]: #not a private message
+        self.queue_reply("TOPIC " + self.params[0])
+        reply = "The channel topic is:"
+        if len(self.trailing):
+            tell_nick, self.trailing = parse.get_word(self.trailing)
+            reply = tell_nick + ", the channel topic is:"
+        line_list = []
+        while 1:
+            line = self.getline()
+            sender, command, params, trailing = self.parse_message(line)
+            self.debugprint("in topic: " + line)
+            if command == constants.RPL_NOTOPIC \
+               and params[1] == self.params[0]:
+                self.privmsg(self.sender[0], "what topic?")
+                break
+            elif command == constants.RPL_TOPIC \
+                 and params[1] == self.params[0]:
+                reply += " " + trailing
+                self.queue_reply("PRIVMSG" + ' ' + self.params[0]
+                           + ' ' + ':' + reply)
+                break
+            else:
+                line_list.append(line)
+        for line in line_list:
+            self.process(line)
